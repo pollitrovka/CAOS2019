bbce59761d06c2b99789f17a69fd6dd3172a6fa3
timofeeva julija vladislavovna <timofeeva.yuv@phystech.edu>
2019-10-02 07:09:25 +0300

    add inf01-2

---
+++ inf01-2/main.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+uint16_t
+satsum(uint16_t x, uint16_t y) {
+    uint16_t sum = x + y;
+    if (sum < x || sum < y) {
+        return (uint16_t)(~0);
+    }
+    return sum;
+}
+
